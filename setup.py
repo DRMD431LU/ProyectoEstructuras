3a820f14d6933328f4d4af3e92e4f441ce29438a
unknown <drmdfoureilu@gmail.com>
2018-05-17 12:09:57 -0500

    updated

---
+++ setup.py
@@ -0,0 +1,23 @@
+from cx_Freeze import setup, Executable
+
+base = None
+
+
+executables = [Executable("main.py", base=base)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {
+
+        'packages':packages,
+    },
+
+}
+
+setup(
+    name = "<any name>",
+    options = options,
+    version = "<any number>",
+    description = '<any description>',
+    executables = executables
+)
